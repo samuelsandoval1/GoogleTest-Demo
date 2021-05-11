9764a85246ec62f0772dab248346d042713d2dd8
Samuel Sandoval <51276320+samuelsandoval1@users.noreply.github.com>
2021-05-11 14:30:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,6 @@
 
 using namespace std;
 
-// TEST(IntegerInputsSuite, simpleSum){
-//     //first, set up any inputs to your
-//     const int SIZE = 3;
-//     double arr[SIZE] = {1, 2, 3};
-//     //then, make an assertion to test
-//     EXPECT_EQ(summer(arr, SIZE), 6) << "The sum is not correct";
-// }
-
-TEST (StackTests, initialize) {
-   Stack<int> s;
-   ASSERT_EQ(s.size(), 0);
-}
-
-TEST (StackTests, push) {
-   Stack<int> s;
-   s.push(5);
-   ASSERT_EQ(s.top(), 5); 
- 
-   s.push(9);
-   ASSERT_EQ(s.top(), 9);
- 
-   s.push(10);
-   ASSERT_EQ(s.size(), 3);
-}
-
-TEST (StackTests, pop_and_top){
-    Stack<int> s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.pop();
-    ASSERT_EQ(s.top() ,2);
-}
 // Implement unit tests to test the functionality of the Stack.h file
 
 int main(int argc, char **argv){
