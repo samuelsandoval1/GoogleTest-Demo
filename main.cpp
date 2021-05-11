911f0c3cd3f73ba25f993d97aa4da8260b2cb9f7
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-05-11 14:29:58 -0700

    test progress

--- main.cpp
+++ main.cpp
@@ -14,15 +14,31 @@ using namespace std;
 //     EXPECT_EQ(summer(arr, SIZE), 6) << "The sum is not correct";
 // }
 
-
-TEST(Stack, pop){
-    Stack<int> s0;
-    s0.push(1);
-    s0.push(2);
-    s0.pop();
-    ASSERT_EQ(s0.top(), 0);
+TEST (StackTests, initialize) {
+   Stack<int> s;
+   ASSERT_EQ(s.size(), 0);
 }
 
+TEST (StackTests, push) {
+   Stack<int> s;
+   s.push(5);
+   ASSERT_EQ(s.top(), 5); 
+ 
+   s.push(9);
+   ASSERT_EQ(s.top(), 9);
+ 
+   s.push(10);
+   ASSERT_EQ(s.size(), 3);
+}
+
+TEST (StackTests, pop_and_top){
+    Stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.pop();
+    ASSERT_EQ(s.top() ,2);
+}
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
